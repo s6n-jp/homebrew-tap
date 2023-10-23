689e92d84e625e712373d241e4087fc0fcde01c8
Natsuki Ikeguchi <me@s6n.jp>
2023-10-23 20:38:13 +0900

    feat(jpki-cli): Bump to v0.4.3

--- Formula/jpki-cli.rb
+++ Formula/jpki-cli.rb
@@ -1,8 +1,8 @@
 class JpkiCli < Formula
   desc "Read certificates, sign and verify documents using your JPKI card"
   homepage "https://github.com/siketyan/jpki-rs"
-  url "https://github.com/siketyan/jpki-rs/archive/refs/tags/0.3.0.tar.gz"
-  sha256 "e42576953c1a83589419b9ff40fde00e07fbd8723cad78d6108be1ffc695fcda"
+  url "https://github.com/siketyan/jpki-rs/archive/refs/tags/0.4.3.tar.gz"
+  sha256 "c7954b0b0f0713a3706ac76e506128fb512c0dab488b71daba33d64d1e379b8f"
   license "LGPL-2.1"
 
   depends_on "rust" => :build
@@ -13,9 +13,6 @@ class JpkiCli < Formula
 
   def install
     system "cargo", "install", *std_cargo_args
-
-    bash_completion.install "resources/shell/bash/ghr-completion.bash"
-    fish_completion.install "resources/shell/fish/ghr-completion.fish"
   end
 
   test do
