e8a7e510a4e757b771e74d791d38d1963e9e2105
Natsuki Ikeguchi <me@s6n.jp>
2023-10-23 12:15:47 +0000

    ghr: add 0.3.11 bottle.

--- Formula/ghr.rb
+++ Formula/ghr.rb
@@ -5,6 +5,15 @@ class Ghr < Formula
   sha256 "d5583898f1d881b63585afe226bb3a83acf389c874aa7ca348662efe4b71bf76"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/s6n-jp/homebrew-tap/releases/download/ghr-0.3.11"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_ventura: "2a7a78f2be4153d6e5353d667d465abb51c7d128443363ece6321769de1d5eda"
+    sha256 cellar: :any,                 ventura:       "9d44ea3c3688201a551126c48de599acd66f21079df5eca9ba69e3840b16b7ff"
+    sha256 cellar: :any,                 monterey:      "df04fa5307568ceb14e7dbc89cc1e88e50afad3cc59f09257f1babd2dd8efbb7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f1b60992dbfe72fa0941bd062dc9309ef446324cf93bcac9d9cd0e6c24d838af"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@3"
 
