5e3f7b7a6851a93a5db4b1f283e1a55840c65f3f
Natsuki Ikeguchi <me@s6n.jp>
2024-01-03 16:44:07 +0900

    fix(qrean): Fix formula style

--- Formula/qrean.rb
+++ Formula/qrean.rb
@@ -1,9 +1,9 @@
 class Qrean < Formula
-  desc "A portable QR and Barcode generation / manipulation library written in C."
+  desc "Portable QR and Barcode generation / manipulation library written in C"
   homepage "https://github.com/kikuchan/libqrean"
-  url "https://github.com/kikuchan/libqrean/archive/4d77933.tar.gz"
-  version "v0.0.0-4d77933"
-  sha256 "9a442c83af29408b6e376c9232f9244122ef6ce8f1b81da10246054571881f97"
+  url "refs/heads/4d77933"
+  version "0.0.0-4d77933"
+  sha256 "f1021db34e41f7a1749672945dd2b77235bd04184376f8ccfff07e613a53685d"
   license "MIT"
 
   depends_on "make" => :build
