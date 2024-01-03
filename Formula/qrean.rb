2b1c90aa6aac068b53ebe6c13a3f5912d5206a79
Natsuki Ikeguchi <me@s6n.jp>
2024-01-03 16:58:48 +0900

    fix(qrean): Use head-only formula

--- Formula/qrean.rb
+++ Formula/qrean.rb
@@ -1,10 +1,8 @@
 class Qrean < Formula
   desc "Portable QR and Barcode generation / manipulation library written in C"
   homepage "https://github.com/kikuchan/libqrean"
-  url "refs/heads/4d77933"
-  version "0.0.0-4d77933"
-  sha256 "f1021db34e41f7a1749672945dd2b77235bd04184376f8ccfff07e613a53685d"
   license "MIT"
+  head "https://github.com/kikuchan/libqrean.git", branch: "main"
 
   depends_on "make" => :build
 
